a4c519b167e7d15113178041a73b2db296a51e65
iFruze <denisbyev4s@gmail.com>
2025-05-22 17:27:12 +0300

    qadasdf

--- VeloKom/WebApplication1/obj/Debug/net9.0/VeloKom.AssemblyInfo.cs
+++ VeloKom/WebApplication1/obj/Debug/net9.0/VeloKom.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VeloKom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce1754b754a8a1f17dbab140745858039c886370")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdf18ba4cba82d045755104185d2ef2f443f6a22")]
 [assembly: System.Reflection.AssemblyProductAttribute("VeloKom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VeloKom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
